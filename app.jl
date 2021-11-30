d7c3904e909324f42e27ce61d7c3a5496b670535
jint <jesse@spire.com>
2021-11-29 19:08:40 -0800

    Reorganize and consolidate code for easier refactoring

---
+++ app.jl
@@ -0,0 +1,17 @@
+import Cairo, Fontconfig
+using POMDPs
+using RockSample 
+using SARSOP # load a  POMDP Solver
+using POMDPGifs # to make gifs
+
+pomdp = RockSamplePOMDP(rocks_positions=[(2,3), (4,4), (4,2)], 
+                        sensor_efficiency=20.0,
+                        discount_factor=0.95, 
+                        good_rock_reward = 20.0)
+
+solver = SARSOPSolver(precision=1e-3)
+
+policy = solve(solver, pomdp)
+
+sim = GifSimulator(filename="test.gif", max_steps=30)
+simulate(sim, pomdp, policy)
--- src/RockSample.jl
+++ src/RockSample.jl
@@ -85,11 +85,153 @@ RockSamplePOMDP(map_size::Tuple{Int, Int}, rocks_positions::AbstractVector) = Ro
 POMDPs.isterminal(pomdp::RockSamplePOMDP, s::RSState) = s.pos == pomdp.terminal_state.pos 
 POMDPs.discount(pomdp::RockSamplePOMDP) = pomdp.discount_factor
 
-include("states.jl")
-include("actions.jl")
-include("transition.jl")
-include("observations.jl")
-include("reward.jl")
+## States
+function POMDPs.stateindex(pomdp::RockSamplePOMDP{K}, s::RSState{K}) where K
+    if isterminal(pomdp, s)
+        return length(pomdp)
+    end
+    return s.pos[1] + pomdp.indices[1] * (s.pos[2]-1) + dot(view(pomdp.indices, 2:(K+1)), s.rocks)
+end
+
+function state_from_index(pomdp::RockSamplePOMDP{K}, si::Int) where K
+    if si == length(pomdp)
+        return pomdp.terminal_state
+    end
+    rocks_dim = fill(2, K)
+    nx, ny = pomdp.map_size
+    s = CartesianIndices((nx, ny, rocks_dim...))[si]
+    pos = RSPos(s[1], s[2])
+    rocks = SVector{K, Bool}([(s[i] - 1) for i=3:K+2])
+    return RSState{K}(pos, rocks)
+end
+
+# the state space is the pomdp itself
+POMDPs.states(pomdp::RockSamplePOMDP) = pomdp
+
+Base.length(pomdp::RockSamplePOMDP) = pomdp.map_size[1]*pomdp.map_size[2]*2^length(pomdp.rocks_positions) + 1
+
+# we define an iterator over it 
+function Base.iterate(pomdp::RockSamplePOMDP, i::Int=1)
+    if i > length(pomdp)
+        return nothing
+    end
+    s = state_from_index(pomdp, i)
+    return (s, i+1)
+end
+
+function POMDPs.initialstate(pomdp::RockSamplePOMDP{K}) where K 
+    probs = normalize!(ones(2^K), 1)
+    states = Vector{RSState{K}}(undef, 2^K)
+    for (i,rocks) in enumerate(Iterators.product(ntuple(x->[false, true], K)...))
+        states[i] = RSState{K}(pomdp.init_pos, SVector(rocks))
+    end
+    return SparseCat(states, probs)
+end
+
+
+## Actions
+const N_BASIC_ACTIONS = 5
+const BASIC_ACTIONS_DICT = Dict(:north => 1, 
+                                :east => 2,
+                                :south => 3,
+                                :west => 4,
+                                :sample => 5)
+
+const ACTION_DIRS = (RSPos(0,1),
+                    RSPos(1,0),
+                    RSPos(0,-1),
+                    RSPos(-1,0),
+                    RSPos(0,0))
+
+POMDPs.actions(pomdp::RockSamplePOMDP{K}) where K = 1:N_BASIC_ACTIONS+K
+POMDPs.actionindex(pomdp::RockSamplePOMDP, a::Int) = a
+
+function POMDPs.actions(pomdp::RockSamplePOMDP{K}, s::RSState) where K
+    if in(s.pos, pomdp.rocks_positions) # slow? pomdp.rock_pos is a vec 
+        return actions(pomdp)
+    else
+        # sample not available
+        return 2:N_BASIC_ACTIONS+K
+    end
+end
+
+## Transitions
+function POMDPs.transition(pomdp::RockSamplePOMDP{K}, s::RSState{K}, a::Int) where K
+    if isterminal(pomdp, s)
+        return Deterministic(pomdp.terminal_state)
+    end
+    new_pos = next_position(s, a)
+    if a == BASIC_ACTIONS_DICT[:sample] && in(s.pos, pomdp.rocks_positions)
+        rock_ind = findfirst(isequal(s.pos), pomdp.rocks_positions) # slow ?
+        # set the new rock to bad
+        new_rocks = MVector{K, Bool}(undef)
+        for r=1:K
+            new_rocks[r] = r == rock_ind ? false : s.rocks[r]
+        end
+        new_rocks = SVector(new_rocks)
+    else 
+        new_rocks = s.rocks
+    end
+    if new_pos[1] > pomdp.map_size[1]
+        # the robot reached the exit area
+        new_state = pomdp.terminal_state
+    else
+        new_pos = RSPos(clamp(new_pos[1], 1, pomdp.map_size[1]), 
+                        clamp(new_pos[2], 1, pomdp.map_size[2]))
+        new_state = RSState{K}(new_pos, new_rocks)
+    end
+    return Deterministic(new_state)
+end
+
+function next_position(s::RSState, a::Int)
+    if a < N_BASIC_ACTIONS
+        # the robot moves 
+        return s.pos + ACTION_DIRS[a]
+    elseif a >= N_BASIC_ACTIONS 
+        # robot check rocks or samples
+        return s.pos
+    else
+        throw("ROCKSAMPLE ERROR: action $a not valid")
+    end
+end
+
+## Observations
+const OBSERVATION_NAME = (:good, :bad, :none)
+
+POMDPs.observations(pomdp::RockSamplePOMDP) = 1:3
+POMDPs.obsindex(pomdp::RockSamplePOMDP, o::Int) = o
+
+function POMDPs.observation(pomdp::RockSamplePOMDP, a::Int, s::RSState)
+    if a <= N_BASIC_ACTIONS
+        # no obs
+        return SparseCat((1,2,3), (0.0,0.0,1.0)) # for type stability
+    else
+        rock_ind = a - N_BASIC_ACTIONS 
+        rock_pos = pomdp.rocks_positions[rock_ind]
+        dist = norm(rock_pos - s.pos)
+        efficiency = 0.5*(1.0 + exp(-dist/pomdp.sensor_efficiency))
+        rock_state = s.rocks[rock_ind]
+        if rock_state
+            return SparseCat((1,2,3), (efficiency, 1.0 - efficiency, 0.0))
+        else
+            return SparseCat((1,2,3), (1.0 - efficiency, efficiency, 0.0))
+        end
+    end
+end
+
+## Rewards
+function POMDPs.reward(pomdp::RockSamplePOMDP, s::RSState, a::Int)
+    if next_position(s, a)[1] > pomdp.map_size[1]
+        return pomdp.exit_reward
+    end
+
+    if a == BASIC_ACTIONS_DICT[:sample] && in(s.pos, pomdp.rocks_positions) # sample 
+        rock_ind = findfirst(isequal(s.pos), pomdp.rocks_positions) # slow ?
+        return s.rocks[rock_ind] ? pomdp.good_rock_reward : pomdp.bad_rock_penalty 
+    end
+    return 0.
+end
+
 include("visualization.jl")
 include("heuristics.jl")
 
